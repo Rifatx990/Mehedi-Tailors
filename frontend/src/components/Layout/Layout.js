100ee319ab97b4cc816aa4e97f2d52c81d7817af
Rifatx990 <158224402+Rifatx990@users.noreply.github.com>
2025-12-17 14:52:39 +0600

    Create Layout.js

---
+++ frontend/src/components/Layout/Layout.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Navbar from '../Layout/Navbar';
+import Footer from '../Layout/Footer';
+import { Toaster } from 'react-hot-toast';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow">
+        {children}
+      </main>
+      <Footer />
+      <Toaster 
+        position="top-right"
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+          success: {
+            duration: 3000,
+            theme: {
+              primary: 'green',
+              secondary: 'black',
+            },
+          },
+          error: {
+            duration: 4000,
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default Layout;
